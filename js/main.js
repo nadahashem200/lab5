3c680c677504df982b98b85b747e62c35eca4bf0
Nada Hashem <nadahashem200@gmail.com>
2025-11-02 01:25:21 -0700

    Added Troy's js guide, added buttons and diclaimer , and styled the page.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+import { cueTimer } from "./modules/cuepoints.js";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+
+    var myCues = [
+        { seconds: 2, callback: func1 },
+        { seconds: 9, callback: func2 },
+        { seconds: 15, callback: func3 }
+    ];
+
+    //this activates the cuepoints module.
+    // Pass it the ID of the video to watch
+    // and the array of cuepoint objects.
+    cueTimer.setup("vid", myCues);
+
+    //shortcut variables
+    const vid = document.querySelector("#vid");
+    const selectList = document.querySelector("#video_select");
+
+    // make the select list control what video format to play
+    selectList.addEventListener("change", (e) => {
+        selectVideo(e, vid);
+    });
+
+});
+
+
+
+//the custom callback functions to trigger when a cuepoint is hit.
+//You can code up whatever behavior you need in your own callbacks
+//feel free to rename the functions to be more descriptive of what they do.
+function func1() {
+
+}
+
+function func2() {
+    
+}
+
+function func3() {
+
+}
